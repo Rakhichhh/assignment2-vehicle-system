43eed5edc423746964fe6624d59f1e59ad662745
Rakhichhh <xw5q8fdgv9@privaterelay.appleid.com>
2025-12-30 12:16:00 +0500

    Add Driver class

---
+++ src/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+
+    private String name;
+    private String licenseNumber;
+
+    public Driver(String name, String licenseNumber) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void displayDriverInfo() {
+        System.out.println("Driver: " + name + ", License: " + licenseNumber);
+    }
+}
