5220c32dafe6f20e08884883070d350bf14a34cc
Rakhichhh <xw5q8fdgv9@privaterelay.appleid.com>
2025-12-30 12:58:49 +0500

    Add Motorcycle class

---
+++ src/Motorcycle.java
@@ -0,0 +1,19 @@
+public class Motorcycle extends Vehicle {
+
+    private boolean hasSidecar;
+
+    public Motorcycle(String brand, int year, boolean hasSidecar) {
+        super(brand, year);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Motorcycle engine started: " + brand);
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Motorcycle engine stopped: " + brand);
+    }
+}
